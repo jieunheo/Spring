f9a524c35d72e5e3b7614a0c95850beb0ea629e9
heojieun <gjekdmlwl@naver.com>
2022-02-08 11:03:04 +0900

    로그인/회원목록보기/회원가입

--- SpringWorkSpace/springtest/src/main/java/com/ezen/myapp/persistance/MemberService.java
+++ SpringWorkSpace/springtest/src/main/java/com/ezen/myapp/persistance/MemberService.java
@@ -2,16 +2,21 @@ package com.ezen.myapp.persistance;
 
 import java.util.ArrayList;
 
-import com.ezen.myapp.model.BoardVo;
+import org.apache.ibatis.annotations.Param;
+
+import com.ezen.myapp.model.MemberVo;
 
 /* 마이바트스용 메서드가 있는 곳 */
 public interface MemberService {
 
-	//글쓰기
-	//public int insertBoard(String subject);
+	//회원 가입
+	public int joinMember(@Param("memberName") String memberName, @Param("memberId") String memberId, @Param("memberPw") String memberPw);
 	
-	//글목록
-	//public ArrayList<BoardVo> selectAllBoard();
+	//회원 로그인
+	public MemberVo loginMember(@Param("memberId") String memberId, @Param("memberPw") String memberPw);
+	
+	//회원 목록
+	public ArrayList<MemberVo> selectAllMember();
 	
 	//글보기
 	//public BoardVo selectOneBoard(String bidx);
